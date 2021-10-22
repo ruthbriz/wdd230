72bb03c0b3223ce395412999637bfd086f0c52bf
ruthbriz <ruthbriz06@gmail.com>
2021-10-22 21:42:41 +0800

    Update windchill.js

---
+++ Lesson 06/js/windchill.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    document.getElementById("calcute");
+if (t <= 10 || s <= 4.8){
+let t = document.querySelector("temp");
+let s = document.querySelector("windspeed");
+let f = 35.74+(0.6215*(t−35.75))*(Math.pow(s,0.16))+0.4275*(t*Math.pow(s, 0.16));
+}
+else{
+    alert("N/A!");
+}
+}
