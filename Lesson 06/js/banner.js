431387edcd03971a1feb2b78d93cd686cd154381
ruthbriz <ruthbriz06@gmail.com>
2021-10-23 15:39:00 +0800

    Update banner.js

--- Lesson 06/js/banner.js
+++ Lesson 06/js/banner.js
@@ -1,15 +1,22 @@
-document.querySelector(".closebtn").addEventListener("click", function () {
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+
+  if (DayName == "Friday"){
+	  document.getElementById("banner");
+  document.querySelector(".closebtn").addEventListener("click", function () {
 	this.closest(".alert").remove();
   });
-  
-
-const d = new Date();
-const daynames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const dayName = daynames[d.getDay()];
-if(dayName == Friday) {
-  document.getElementById("banner").style.display = "block";
-  banner.remove();
 }
-else{
-	document.getElementById("banner").style.display = "none";
-}
+  else{
+	  document.getElementById("banner").style.display = "none";
+	  )
+  }
