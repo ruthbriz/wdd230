c90a500e50b7352143357c384ca67f66f7ae8960
ruthbriz <ruthbriz06@gmail.com>
2021-10-23 15:58:33 +0800

    Update banner.js

--- Lesson 06/js/banner.js
+++ Lesson 06/js/banner.js
@@ -1,22 +1,7 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-
-  if (DayName == "Friday"){
-	  document.getElementById("banner");
-  document.querySelector(".closebtn").addEventListener("click", function () {
-	this.closest(".alert").remove();
+WebFont.load({
+    google: {
+      families: [
+         "Oswald, Lato"
+      ]
+    }
   });
-}
-  else{
-	  document.getElementById("banner").style.display = "none";
-	  )
-  }
