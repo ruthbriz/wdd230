8c1efbaa225c831777a30653b524b9ea6e795405
ruthbriz <ruthbriz06@gmail.com>
2021-10-28 15:25:37 +0800

    Update lazyload.js

--- Lesson 07/lazyload.js
+++ Lesson 07/lazyload.js
@@ -2,29 +2,29 @@ const imagestoLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 100px 0px"
+    rootMargin: "0px 0px 50px 0px"
 };
 
-const loadImages = (images) => {
+const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');};
 };
 
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
         if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
+            loadImages(item.target);
+            observer.unobserve(item.target);
         }
+        });
     });
-}, imgOptions);
-imagestoLoad.forEach((img) => {
-observer.observe(img);
-});
-} else{
     imagestoLoad.forEach((img) => {
-     loadImages(img);
+        observer.observe(img);
     });
-}
+    } else {
+    imagestoLoad.forEach((img) => {
+        loadImages(img);
+    });
+    }    
