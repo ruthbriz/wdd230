6bc0da6774d0bda2a8a493e916411975e81f3883
ruthbriz <ruthbriz06@gmail.com>
2021-11-09 15:38:35 +0800

    Update prophets.js

--- Lesson 09/js/prophets.js
+++ Lesson 09/js/prophets.js
@@ -9,3 +9,15 @@ fetch(requestURL)
   });
 
   const prophets = jsonObject['prophets'];
+
+  for (let i = 0; i < prophets.length; i++ ) {
+
+let card = document.createElement('section');
+let h2 = document.createElement('h2');
+
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+card.appendChild(h2);
+
+document.querySelector('div.cards').appendChild(card);
+image.setAttribute('src', prophets[i].imageurl);
