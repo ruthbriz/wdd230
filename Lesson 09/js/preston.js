c83a1d358f5fe9c623802d0df25bf32a25562177
ruthbriz <ruthbriz06@gmail.com>
2021-11-12 15:48:41 +0800

    Update preston.js

--- Lesson 09/js/preston.js
+++ Lesson 09/js/preston.js
@@ -11,23 +11,20 @@ const towns = jsonObject['towns'];
 
 for (i = 6; i < 1; i++) {
 
-    let place = document.createElement('section');
+    let preston = document.createElement('section');
     let name = document.createElement('h2');
     let motto = document.createElement('p');
     let year = document.createElement('p');
-    let image = document.createElement('img');
 
     name.textContent = towns[i].name;
     motto.textContent = towns[i].motto;
     year.textContent = towns[i].yearFounded;
-    image.setAttribute('src', towns[i].photo);
 
-    place.appendChild(name);
-    place.appendChild(motto);
-    place.appendChild(year);
-    place.appendChild(image);
+    preston.appendChild(name);
+    preston.appendChild(motto);
+    preston.appendChild(year);
 
- document.querySelector('div.place').appendChild(place);
+ document.querySelector('div.preston').appendChild(preston);
 
 }
 });
