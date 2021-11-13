5390a4a8282bab4ad4cf9cb631a0752f69dbe6b0
ruthbriz <ruthbriz06@gmail.com>
2021-11-13 10:24:01 +0800

    Update json.js

--- Lesson 09/js/json.js
+++ Lesson 09/js/json.js
@@ -9,35 +9,70 @@ fetch(towndata)
 
 const towns = jsonObject['towns'];
 
-for (i = 0; i < 3; i++) {
+for (i = 0; i < 1; i++) {
 
-    let springsoda = document.createElement('section');
-    let springname = document.createElement('h2');
-    let springmotto = document.createElement('p');
-    let springyear = document.createElement('p');
+    let sodasprings = document.createElement('section');
+    let sodaname = document.createElement('h2');
+    let sodamotto = document.createElement('p');
+    let sodayear = document.createElement('p');
+    let sodapopulation = document.createElement('p');
+    let sodaannualrain = document.createElement('p');
 
-    springname.textContent = towns[0].name;
-    springmotto.textContent = '"' + towns[0].motto + '"';
-    springyear.textContent = 'Year Founded: ' + towns[0].yearFounded;
+    sodaname.textContent = towns[0].name;
+    sodamotto.textContent = '"' + towns[0].motto + '"';
+    sodayear.textContent = 'Year Founded: ' + towns[0].yearFounded;
+    sodapopulation.textContent = 'Population: ' + towns[0].currentPopulation;
+    sodaannualrain.textContent = 'Annual Rain Fall: ' + towns[0].averageRainfall;
 
-    springsoda.appendChild(springname);
-    springsoda.appendChild(springmotto);
-    springsoda.appendChild(springyear);
+    sodasprings.appendChild(sodaname);
+    sodasprings.appendChild(sodamotto);
+    sodasprings.appendChild(sodayear);
+    sodasprings.appendChild(sodapopulation);
+    sodasprings.appendChild(sodaannualrain);
+
+    document.querySelector('div.sodasprings').appendChild(sodasprings);
 
     let fishhaven = document.createElement('section');
     let fishname = document.createElement('h2');
     let fishmotto = document.createElement('p');
     let fishyear = document.createElement('p');
+    let fishpopulation = document.createElement('p');
+    let fishannualrain = document.createElement('p');
 
-    fishname.textContent = towns[1].name;
-    fishmotto.textContent = '"' + towns[1].motto + '"';
-    fishyear.textContent = 'Year Founded: ' + towns[1].yearFounded;
+    fishname.textContent = towns[2].name;
+    fishmotto.textContent = '"' + towns[2].motto + '"';
+    fishyear.textContent = 'Year Founded: ' + towns[2].yearFounded;
+    fishpopulation.textContent = 'Population: ' + towns[2].currentPopulation;
+    fishannualrain.textContent = 'Annual Rain Fall: ' + towns[2].averageRainfall;
 
     fishhaven.appendChild(fishname);
     fishhaven.appendChild(fishmotto);
     fishhaven.appendChild(fishyear);
+    fishhaven.appendChild(fishpopulation);
+    fishhaven.appendChild(fishannualrain);
 
- document.querySelector('div.place').appendChild(place);
+    document.querySelector('div.fishhaven').appendChild(fishhaven);
+    
+    let preston = document.createElement('section');
+    let prestonname = document.createElement('h2');
+    let prestonmotto = document.createElement('p');
+    let prestonyear = document.createElement('p');
+    let prestonpopulation = document.createElement('p');
+    let prestonannualrain = document.createElement('p');
+
+    prestonname.textContent = towns[6].name;
+    prestonmotto.textContent = '"' + towns[6].motto + '"';
+    prestonyear.textContent = 'Year Founded: ' + towns[6].yearFounded;
+    prestonpopulation.textContent = 'Population: ' + towns[6].currentPopulation;
+    prestonannualrain.textContent = 'Annual Rain Fall: ' + towns[6].averageRainfall;
+
+    preston.appendChild(prestonname);
+    preston.appendChild(prestonmotto);
+    preston.appendChild(prestonyear);
+    preston.appendChild(prestonpopulation);
+    preston.appendChild(prestonannualrain);
+
+    document.querySelector('div.preston').appendChild(preston);
 
 }
 });
