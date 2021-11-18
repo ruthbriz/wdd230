7eb08da4276267ba44ce971d134cd77e5deda1a3
ruthbriz <ruthbriz06@gmail.com>
2021-11-18 16:53:23 +0800

    Update weatherapi.js

--- Lesson 10/js/weatherapi.js
+++ Lesson 10/js/weatherapi.js
@@ -1,9 +1,9 @@
 const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
+.then((response) => response.json())
+.then((jsObject) => {
+  console.log(jsObject);
 
-    
     document.getElementById('current-temp').textContent = jsObject.main.temp;
+
   });
