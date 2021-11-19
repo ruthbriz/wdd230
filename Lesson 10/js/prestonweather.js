854fef67b5c4b3bc65ae48786db4b385983cedef
ruthbriz <ruthbriz06@gmail.com>
2021-11-19 20:10:30 +0800

    Update prestonweather.js

--- Lesson 10/js/prestonweather.js
+++ Lesson 10/js/prestonweather.js
@@ -16,18 +16,7 @@ fetch(apiURL)
   console.log(jsObject);
   
   document.getElementById('current').textContent = jsObject.weather[0].main;
-  document.getElementById('high').textContent = jsObject.wind.gust;
+  document.getElementById('high').textContent = jsObject.main.temp;
   document.getElementById('humidity').textContent = jsObject.main.humidity;
   document.getElementById('windspeed').textContent = jsObject.wind.speed;
-
-  let t = document.querySelector("high");
-let s = document.querySelector("windspeed");
-
-if (t <= 10 && s >= 4.8){
-let f = 35.74+(0.6215*t)-35.75*(Math.pow(s,0.16))+0.4275*(t*Math.pow(s, 0.16));
-document.getElementById("windchill").innerHTML = f + " °F";
-}
-else {
-document.getElementById("windchill").innerHTML = "N/A!";
-}
   });
