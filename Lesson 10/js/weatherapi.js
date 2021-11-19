036cb4ea684ea2af4cfbb99cb1672c597856640f
ruthbriz <ruthbriz06@gmail.com>
2021-11-19 18:42:27 +0800

    Update weatherapi.js

--- Lesson 10/js/weatherapi.js
+++ Lesson 10/js/weatherapi.js
@@ -1,6 +1,14 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
   console.log(jsObject);
+  
+  document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
   });
