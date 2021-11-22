b6fe93d83f8618f78fe2b0ce57da46cddc69d272
ruthbriz <ruthbriz06@gmail.com>
2021-11-22 10:11:13 +0800

    Update weatherforecast.js

--- Lesson 10/js/weatherforecast.js
+++ Lesson 10/js/weatherforecast.js
@@ -5,24 +5,36 @@ fetch(forecast)
   console.log(jsObject);
 
   const daynames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  const dayNames = daynames[jsObject.list[0].dt_txt];
-  document.getElementById("mon").textContent = dayNames;
+  const monday = daynames[jsObject.list[0].dt_txt];
+  document.getElementById("mon").textContent = monday;
   document.getElementById('day1').textContent = jsObject.list[0].main.temp;
   const weather1 = 'http://openweathermap.org/img/wn/' + jsObject.list[0].weather[0].icon + '@2x.png';
   document.getElementById('weather1').setAttribute('src', weather1);
 
+  const daynames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const tuesday = daynames[jsObject.list[0].dt_txt];
+  document.getElementById("mon").textContent = tuesday;
   document.getElementById('day2').textContent = jsObject.list[1].main.temp;
   const weather2 = 'http://openweathermap.org/img/wn/' + jsObject.list[1].weather[0].icon + '@2x.png';
   document.getElementById('weather2').setAttribute('src', weather2);
 
+  const daynames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const wednesday = daynames[jsObject.list[0].dt_txt];
+  document.getElementById("mon").textContent = wednesday;
   document.getElementById('day3').textContent = jsObject.list[2].main.temp;
   const weather3 = 'http://openweathermap.org/img/wn/' + jsObject.list[2].weather[0].icon + '@2x.png';
   document.getElementById('weather3').setAttribute('src', weather3);
 
+  const daynames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const thursday = daynames[jsObject.list[0].dt_txt];
+  document.getElementById("mon").textContent = thursday;
   document.getElementById('day4').textContent = jsObject.list[3].main.temp;
   const weather4 = 'http://openweathermap.org/img/wn/' + jsObject.list[3].weather[0].icon + '@2x.png';
   document.getElementById('weather4').setAttribute('src', weather4);
 
+  const daynames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const friday = daynames[jsObject.list[0].dt_txt];
+  document.getElementById("mon").textContent = friday;
   document.getElementById('day5').textContent = jsObject.list[4].main.temp;
   const weather5 = 'http://openweathermap.org/img/wn/' + jsObject.list[4].weather[0].icon + '@2x.png';
   document.getElementById('weather5').setAttribute('src', weather5);
