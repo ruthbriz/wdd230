4669f64566c5bf0aa765c2f65abdaa4d8fe3b806
ruthbriz <ruthbriz06@gmail.com>
2021-11-24 16:03:20 +0800

    Update fishhavenevent.js

--- Lesson 11/js/fishhavenevent.js
+++ Lesson 11/js/fishhavenevent.js
@@ -12,17 +12,20 @@ const towns = jsonObject['towns'];
 for (i = 0; i < 1; i++) {
 
     let town = document.createElement('div')
-    let sodaeventone = document.createElement('p');
-    let sodaeventtwo = document.createElement('p');
-    let sodaeventthree = document.createElement('p');
+    let fisheventone = document.createElement('p');
+    let fisheventtwo = document.createElement('p');
+    let fisheventthree = document.createElement('p');
+    let fisheventfour = document.createElement('p');
 
-    sodaeventone.textContent = towns[0].events[0];
-    sodaeventtwo.textContent = towns[0].events[1];
-    sodaeventthree.textContent = towns[0].events[2];
+    fisheventone.textContent = towns[2].events[0];
+    fisheventtwo.textContent = towns[2].events[1];
+    fisheventthree.textContent = towns[2].events[2];
+    fisheventfour.textContent = towns[2].events[3];
 
-    town.appendChild(sodaeventone);
-    town.appendChild(sodaeventtwo);
-    town.appendChild(sodaeventthree);
+    town.appendChild(fisheventone);
+    town.appendChild(fisheventtwo);
+    town.appendChild(fisheventthree);
+    town.appendChild(fisheventfour);
 
     document.querySelector('div.town').appendChild(town);
 
