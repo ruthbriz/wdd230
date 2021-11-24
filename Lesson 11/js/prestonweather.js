6b9d214366f47f84806766da43a3ea60ab1b9422
ruthbriz <ruthbriz06@gmail.com>
2021-11-24 10:41:00 +0800

    Update prestonweather.js

--- Lesson 11/js/prestonweather.js
+++ Lesson 11/js/prestonweather.js
@@ -1,14 +1,14 @@
 let t = document.querySelector("high");
 let s = document.querySelector("windspeed");
 
-if (t <= 10 && s >= 4.8){
+if (t <= 10 && s >= 4.8 || t <= 50 && s >= 3.0){
 let f = 35.74+(0.6215*t)-35.75*(Math.pow(s,0.16))+0.4275*(t*Math.pow(s, 0.16));
 document.getElementById("windchill").innerHTML = f + " °C";
 }
 else {
 document.getElementById("windchill").innerHTML = "N/A!";
 }
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=metric&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
