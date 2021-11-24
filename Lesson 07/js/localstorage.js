12f59b32f4323ab4128a99dd3042ddbc42fab681
ruthbriz <ruthbriz06@gmail.com>
2021-11-24 12:43:46 +0800

    Update localstorage.js

--- Lesson 07/js/localstorage.js
+++ Lesson 07/js/localstorage.js
@@ -1,16 +1,11 @@
-function countVisits(){
-    str_count = localStorage.getItem("count");
-    //get a numeric value from str_count, put it in count
-    if (str_count == null || str_count == "null"){
-      count = 0;
-    } else {
-      count = parseInt(str_count);
-    } // end if
-    //increment count
-    count++;
-    //display count
-    lblCounter = document.getElementById("lblCounter");
-    lblCounter.innerHTML = "You have been here " + count + " times";
-    //store count
-    localStorage.setItem("count", count);
-  } // end count
+var n = localStorage.getItem('on_load_counter');
+ 
+if (n === null) {
+    n = 0;
+}
+ 
+n++;
+ 
+localStorage.setItem("on_load_counter", n);
+ 
+document.getElementById('counter').innerHTML = n;
