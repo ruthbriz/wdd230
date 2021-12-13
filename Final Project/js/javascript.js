ff6881537f557858ffb20df58e35c85fdbcbe53f
ruthbriz <ruthbriz06@gmail.com>
2021-12-13 09:17:36 +0800

    Update javascript.js

--- Final Project/js/javascript.js
+++ Final Project/js/javascript.js
@@ -3,10 +3,19 @@ function toggleMenu(){
 }
 const daynames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
 const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
 document.getElementById("currentdate").textContent = fulldate;
+document.querySelector(".closebtn").addEventListener("click", function () {
+	this.closest(".alert").remove();
+  });
+  
+  const mon = new Date();
+  if(mon.getDay() != 1) {
+	const banner = document.querySelector("#banner")
+	banner.remove()
+  }
