c0fbe554fd050e78de60e56acc6d0cd63f38fe9d
ruthbriz <ruthbriz06@gmail.com>
2021-12-16 03:13:56 +0800

    Create weather.js

---
+++ Final Project/js/weather.js
@@ -0,0 +1,10 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Sta. Barbara,PH&units=imperial&appid=94c91cdf1598c682eab6d60fbe9ff1bf";
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) => {
+  console.log(jsObject);
+  
+  document.getElementById('current').textContent = jsObject.weather[0].main;
+  document.getElementById('high').textContent = jsObject.main.temp;
+  document.getElementById('humidity').textContent = jsObject.main.humidity;
+  });
