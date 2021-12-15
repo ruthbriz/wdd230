4e3fe6b6baebf16fdcef59bec4077e1904dcc2cc
ruthbriz <ruthbriz06@gmail.com>
2021-12-16 00:22:40 +0800

    Update directory.js

--- Final Project/js/directory.js
+++ Final Project/js/directory.js
@@ -8,24 +8,51 @@ fetch(data)
     console.table(jsonObject);
     const businesses = jsonObject['businesses'];
 
-for (let i = 0; i < businesses.length; i++ ) {
+for (let i = 0; i < 1; i++ ) {
 
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let website = document.createElement('p');
-    let contact = document.createElement('p');
-    let image = document.createElement('img');
+    let jmacks = document.createElement('section');
+    let jmacksname = document.createElement('h2');
+    let jmackswebsite = document.createElement('p');
+    let jmackscontact = document.createElement('p');
+
+    jmacksname.textContent = businesses[0].name;
+    jmackswebsite.textContent = 'Website'+ ': ' + businesses[0].website;
+    jmackscontact.textContent = 'Contact' + ': ' + businesses[0].contact;
+
+    jmacks.appendChild(h2);
+    jmacks.appendChild(website);
+    jmacks.appendChild(contact);
     
-    h2.textContent = businesses[i].name;
-    website.textContent = 'Website'+ ': ' + businesses[i].website;
-    contact.textContent = 'Contact' + ': ' + businesses[i].contact;
-    image.setAttribute('src', businesses[i].imageurl);
+    document.querySelector('div.jmacks').appendChild(jmacks);
+
+    let nursery = document.createElement('section');
+    let nurseryname = document.createElement('h2');
+    let nurserywebsite = document.createElement('p');
+    let nurserycontact = document.createElement('p');
+
+    nurseryname.textContent = businesses[1].name;
+    nurserywebsite.textContent = 'Website'+ ': ' + businesses[1].website;
+    nurserycontact.textContent = 'Contact' + ': ' + businesses[1].contact;
+
+    nursery.appendChild(h2);
+    nursery.appendChild(website);
+    nursery.appendChild(contact);
     
-    card.appendChild(h2);
-    card.appendChild(website);
-    card.appendChild(contact);
-    card.appendChild(image);
+    document.querySelector('div.nursery').appendChild(nursery);
+
+    let bricks = document.createElement('section');
+    let bricksname = document.createElement('h2');
+    let brickswebsite = document.createElement('p');
+    let brickscontact = document.createElement('p');
+
+    bricksname.textContent = businesses[2].name;
+    brickswebsite.textContent = 'Website'+ ': ' + businesses[2].website;
+    brickscontact.textContent = 'Contact' + ': ' + businesses[2].contact;
+
+    bricks.appendChild(h2);
+    bricks.appendChild(website);
+    bricks.appendChild(contact);
     
-    document.querySelector('div.cards').appendChild(card);
+    document.querySelector('div.bricks').appendChild(bricks);
     }
     });
