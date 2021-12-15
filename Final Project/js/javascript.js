1d2e5fa599ded6556eddae26253d2ecef4072e81
ruthbriz <ruthbriz06@gmail.com>
2021-12-16 07:56:41 +0800

    Update javascript.js

--- Final Project/js/javascript.js
+++ Final Project/js/javascript.js
@@ -1,32 +1,22 @@
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
 }
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
+const daynames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.getElementById("currentdate").textContent = fulldate;
+
+document.querySelector(".closebtn").addEventListener("click", function () {
+	this.closest(".alert").remove();
+  });
+  
+let current = document.querySelector('current');
+  if(current == 'Clouds') {
+	const banner = document.querySelector("#banner")
+	banner.remove()
+  }
