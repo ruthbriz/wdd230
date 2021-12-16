69795c124acc1fc6bc7a4a9de18ae4a415393006
ruthbriz <ruthbriz06@gmail.com>
2021-12-16 11:04:37 +0800

    Update javascript.js

--- Final Project/js/javascript.js
+++ Final Project/js/javascript.js
@@ -19,3 +19,6 @@ document.querySelector(".closebtn").addEventListener("click", function () {
 	const banner = document.querySelector("#banner")
 	banner.remove()
   }
+
+  var x = document.lastModified;
+  document.getElementById("lastmodified").innerHTML = x;
