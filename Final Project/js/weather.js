53729e77109d642151458d3cef37a3ec6d9c3a4e
ruthbriz <ruthbriz06@gmail.com>
2021-12-16 08:09:17 +0800

    Update weather.js

--- Final Project/js/weather.js
+++ Final Project/js/weather.js
@@ -7,4 +7,14 @@ fetch(apiURL)
   document.getElementById('current').textContent = jsObject.weather[0].main;
   document.getElementById('high').textContent = jsObject.main.temp;
   document.getElementById('humidity').textContent = jsObject.main.humidity;
+
+  document.querySelector(".closebtn").addEventListener("click", function () {
+    this.closest(".alert").remove();
+    });
+    
+  let current = document.getElementById('current');
+    if(current ==! 'Rainy') {
+    const banner = document.querySelector("#banner")
+    banner.remove()
+    }
   });
